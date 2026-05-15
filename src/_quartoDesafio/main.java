bc93ae8209ba9d7a55c6c12eab7cc58efce1cdce
pedrodamassa701 <pedrocostafalcao7@gmail.com>
2026-05-15 11:33:12 -0300

    mais objetos

--- src/_quartoDesafio/main.java
+++ src/_quartoDesafio/main.java
@@ -3,6 +3,16 @@ package _quartoDesafio;
 public class main {
     public static void main(String[] args) {
 
+        ninjaBasico RockLee = new ninjaBasico("Rock Lee", 8, tipoHabilidade.TAIJUTSU);
+
+        RockLee.mostrarInformacoes();
+        RockLee.executarHabilidade();
+
+        ninjaBasico Neji = new ninjaBasico("Neji Hyuuga", 8, tipoHabilidade.NINJUTSU);
+
+        Neji.mostrarInformacoes();
+        Neji.executarHabilidade();
+
         ninjaBasico Narutinho = new ninjaBasico("Naruto Uzumaki",8,tipoHabilidade.NINJUTSU);
 
         Narutinho.mostrarInformacoes();
